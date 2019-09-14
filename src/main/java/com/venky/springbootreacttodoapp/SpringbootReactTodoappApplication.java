93026061cdc77cd791c01ee4dd82c9910b52086c
vuong.tran <vuong.khanh@digicommercegroup.com>
2019-09-14 09:50:02 +0700

    first commit

---
+++ src/main/java/com/venky/springbootreacttodoapp/SpringbootReactTodoappApplication.java
@@ -0,0 +1,16 @@
+package com.venky.springbootreacttodoapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class SpringbootReactTodoappApplication
+{
+
+	public static void main(String[] args)
+	{
+		SpringApplication.run(SpringbootReactTodoappApplication.class, args);
+	}
+
+}
